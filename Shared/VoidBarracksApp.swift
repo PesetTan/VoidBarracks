b037d5efea60ff327959f9b7d6f955dc0e40314e
Peset Sam Tan <pesettan@gmail.com>
2020-06-29 15:42:11 -0700

    Initial Commit

---
+++ Shared/VoidBarracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidBarracksApp.swift
+//  Shared
+//
+//  Created by Peset Tan on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct VoidBarracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
